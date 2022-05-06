3c4a285770b9d11f964176d1a903fc8feebc8bca
HO <love47024702@naver.com>
2022-05-06 15:08:29 +0900

    데이터 초기화 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,27 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.time.LocalDate;
+import java.time.Month;
+
 public class Main {
     public static void main(String[] args) {
         DBInit dbInit = new DBInit();
 
-//        dbInit.initApartment();
-//        dbInit.initRowHouse();
-//        dbInit.initDetachedHouse();
+//        LocalDate startDate = LocalDate.of(2022, 1, 1); // dayOfMonth는 무조건 1로 설정
+//        LocalDate endDate = LocalDate.of(2022,5,1); // dayOfMonth는 무조건 1로 설정
 
-        dbInit.initRegion();
-        dbInit.initPopulation();
+
+//        dbInit.initRegion();
+//        dbInit.initPopulation();
+//       dbInit.initApartment(startDate,endDate,"11110","DxPzuHVkt4yG1p%2FSpF%2FXChtnOm3jcdhJunTY%2F87a9cRE%2FNc2XYWJ1hgyZHZmpdF6g2ZWaHYGSD%2F5LioW0lc0DQ%3D%3D");
+//       dbInit.initRowHouse(startDate,endDate,"11110","DxPzuHVkt4yG1p%2FSpF%2FXChtnOm3jcdhJunTY%2F87a9cRE%2FNc2XYWJ1hgyZHZmpdF6g2ZWaHYGSD%2F5LioW0lc0DQ%3D%3D");
+//       dbInit.initDetachedHouse(startDate,endDate,"11110", "DxPzuHVkt4yG1p%2FSpF%2FXChtnOm3jcdhJunTY%2F87a9cRE%2FNc2XYWJ1hgyZHZmpdF6g2ZWaHYGSD%2F5LioW0lc0DQ%3D%3D");
+
     }
+
 }
